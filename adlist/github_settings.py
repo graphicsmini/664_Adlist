92a6d585ab677b7e61fa9999263f518ec770a5ec
Youngmin Kim <youngmnk@umich.edu>
2019-04-05 13:41:53 -0400

    first commit

---
+++ adlist/github_settings.py
@@ -0,0 +1,18 @@
+
+
+# Copy this file to github_settings.py (don't check it into github)
+
+# Go to https://github.com/settings/developers
+
+# Add a New OAuth2 App - GIve it a name
+
+# If you are running on localhost, make the callback url be
+# http://127.0.0.1:8000/oauth/complete/github/
+
+# If you are on the real internet (or using ngrok) make the callback url be
+# https://samples.dj4e.com/oauth/complete/github/
+
+# Then copy the client_key and secret to this file
+
+SOCIAL_AUTH_GITHUB_KEY = '36d31b300d425f81f4a4'
+SOCIAL_AUTH_GITHUB_SECRET = 'bb357302788c955e8fa411440f74044bd941ef09'
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/youngminkim/Desktop/664_django/django_projects/adlist/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
